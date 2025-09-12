dd7c384a6d255abc4fa72c8641cf687a2b2f0440
Sara <sara.guellil@elev.ga.lbs.se>
2025-09-12 09:41:43 +0200

    script stuff

--- Call of The Aztecs/Assets/Scripts/Abdullah/CameraFollow.cs
+++ Call of The Aztecs/Assets/Scripts/Abdullah/CameraFollow.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()

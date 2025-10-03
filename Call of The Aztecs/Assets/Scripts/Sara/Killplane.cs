fb79983613a50b5af652bfc62e8e3751b9a33a2e
Sara <sara.guellil@elev.ga.lbs.se>
2025-10-03 10:34:04 +0200

    more fix

---
+++ Call of The Aztecs/Assets/Scripts/Sara/Killplane.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Killplane : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Killplane"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}

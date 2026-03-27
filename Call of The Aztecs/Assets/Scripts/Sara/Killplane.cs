b1dfa573c0e4ba070e3bf08d377b0e74d391b931
Sara <sara.guellil@elev.ga.lbs.se>
2026-03-27 12:05:15 +0100

    killplane

--- Call of The Aztecs/Assets/Scripts/Sara/Killplane.cs
+++ Call of The Aztecs/Assets/Scripts/Sara/Killplane.cs
@@ -4,7 +4,6 @@ public class Killplane : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("enterrd: " + other.gameObject.name + " | tag: " + other.tag);
         TryKillPlayer(other.gameObject);
     }
 
@@ -16,25 +15,10 @@ public class Killplane : MonoBehaviour
     private void TryKillPlayer(GameObject obj)
     {
         if (obj == null) return;
+
         if (obj.CompareTag("Player"))
         {
-            if (CheckpointManager.Instance != null)
-            {
-                CheckpointManager.Instance.RespawnPlayer(obj);
-            }
-            else
-            {
-                Debug.LogWarning("CheckpointManager not found in scene.");
-            }
-
-            return;
-        }
-
-        var entityHealth = obj.GetComponent<playerHealth>() ?? obj.GetComponentInParent<playerHealth>();
-
-        if (entityHealth != null)
-        {
-            entityHealth.TakeDamage(float.MaxValue);
+            Destroy(obj);
         }
     }
 }
